7edee715e69a8012369499406eeb8776cefcc016
Himanshu kaushik <rtxhoney@duck.com>
2023-12-08 18:19:31 +0530

    animations added succeed

--- script.js
+++ script.js
@@ -23,3 +23,30 @@ document.addEventListener('DOMContentLoaded', function () {
   // Add event listener to handle changes in online status
   window.addEventListener('online', handleOnlineStatus);
   window.addEventListener('offline', handleOnlineStatus);
+
+
+   // Array of text options
+   const textOptions = ['Web Developer', 'Video Editor', 'Designer'];
+
+   // Get the h2 element
+   const dynamicTextElement = document.getElementById('dynamicText');
+
+   // Set an index variable to keep track of the current text
+   let currentIndex = 0;
+
+   // Function to update the text with animation
+   function updateText() {
+       dynamicTextElement.classList.remove('animate__fadeIn'); // Remove previous animation class
+
+       // Add animation class, wait for the animation to complete, then update the text
+       dynamicTextElement.classList.add('animate__fadeOut');
+       setTimeout(() => {
+           dynamicTextElement.textContent = textOptions[currentIndex];
+           currentIndex = (currentIndex + 1) % textOptions.length;
+           dynamicTextElement.classList.remove('animate__fadeOut');
+           dynamicTextElement.classList.add('animate__fadeIn');
+       }, 500); // Adjust the delay as needed
+   }
+
+   // Update the text every 3 seconds (adjust the interval as needed)
+   setInterval(updateText, 3000);
